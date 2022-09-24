effd5a23953fe8dc231089ddf9bd899553a39ae8
xamanDawud <ahmeddaudking100@gmail.com>
2022-09-24 12:48:44 +0600

    explored componenet and also have making it

--- src/App.js
+++ src/App.js
@@ -4,21 +4,34 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>HEllo JSX</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Shapes></Shapes>
+      <Shapes></Shapes>
+      <Circle></Circle>
+      <Circle></Circle>
+    </div>
+  );
+}
+
+function Shapes() {
+  return (
+    <div className="xdStyle">
+      <h1>Xaman Dawud</h1>
+      <h2>Xaman Dawud Pro</h2>
+    </div>
+  );
+}
+
+function Circle() {
+  return (
+    <div className="circle_style">
+      <h1>Lorem Circle</h1>
+      <p>
+        Description:
+        <span>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non ratione
+          rem quod nostrum cupiditate quia est aut sequi perspiciatis mollitia?
+        </span>
+      </p>
     </div>
   );
 }
